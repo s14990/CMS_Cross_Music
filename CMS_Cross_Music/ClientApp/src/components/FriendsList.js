53ce988ca8e0f9018a37be33cb9f4bd44d96903a
s14990 <s14990@pjwtk.edu.pl>
2019-11-18 22:10:03 +0100

    added massage

--- CMS_Cross_Music/ClientApp/src/components/FriendsList.js
+++ CMS_Cross_Music/ClientApp/src/components/FriendsList.js
@@ -4,80 +4,89 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 const DUMMY_DATA = [
-  {
-    id_user: 2,
-    user_name:'Pier'
-  },
-  {
-    id_user: 3,
-    user_name:'John'
-  }
+    {
+        id_user: 2,
+        user_name: 'Pier'
+    },
+    {
+        id_user: 3,
+        user_name: 'John'
+    }
 ]
 
 
 class FriendsList extends Component {
 
-  constructor (props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.handleClick = this.handleClick.bind(this);
+        this.handleClick = this.handleClick.bind(this);
 
-    this.state = {
-        activeItemId: -1,
-        ufl: [],
-        friendlist: []
-    };
+        this.state = {
+            activeItemId: -1,
+            ufl: [],
+            friendlist: []
+        };
     }
 
 
-    componentDidMount() {
-        fetch('friendlists?$expand=ufl($expand=userIdUserNavigation)&$select=Ufl&$filter=UserIdUser eq 1' + this.props.auth.idUser)
-          .then(response => response.json())
+    async componentDidMount() {
+        let user_id = this.props.auth.user.idUser;
+        await fetch('api/friendlists?$expand=ufl($expand=userIdUserNavigation)&$select=Ufl&$filter=UserIdUser eq ' + user_id)
+            .then(response => response.json())
             .then(data => {
-              this.setState({
-                  ufl: data[0]
-              });
+                this.setState({
+                    ufl: data[0].Ufl
+                });
 
-          });
+            });
+
+
         var user_list = [];
-        this.sate.ufl.map(uf => {
-            let u;
-            u.id_user = uf.UserUserIdUserNavigation.idUser;
-            u.user_name = uf.UserUserIdUserNavigation.UserName;
+        var list = this.state.ufl;
+        
+        console.log(this.state.ufl);
+        for (var i = 0; i < list.length; i++) {
+            let uf = list[i];
+            let u = {
+                id_user: uf.UserIdUserNavigation.IdUser,
+                user_name: uf.UserIdUserNavigation.UserName
+            }
             user_list.push(u);
-        });
+        }
         this.setState({ friendlist: user_list });
-  }
+
+    }
 
 
-  handleClick(id) {
-     //   console.log('handleClick:', {idTopic, headLine});
-        this.setState({ 
-          activeItemId: id,
+    handleClick(id) {
+        //   console.log('handleClick:', {idTopic, headLine});
+        this.setState({
+            activeItemId: id,
         });
-     //   console.log('isPrivate, state:', this.state);
+        //   console.log('isPrivate, state:', this.state);
 
-  }
-  render() {
-      return (
-        <div className='border rounded'>
-          <p className='text-dark'>FriendsList</p>
-              <ListGroup>
-                  {this.state.friendlist.map((friend, index) => {
-                return (
-              <ListGroupItem  active= {this.state.activeItemId === friend.id_user} color={this.state.activeItemId === friend.id_user?'light':null}
-                key={friend.id_user} tag={Link} to={`/messages/${friend.id_user}` }
-                action onClick={()=> this.handleClick(friend.id_user)}
-              > 
-                {friend.user_name}
-              </ListGroupItem >
-            )               
-            })
-          }
-          </ListGroup>
-        </div>
-      )
-  }
+    }
+    render() {
+        return (
+            <div className='border rounded'>
+                <p className='text-dark'>FriendsList</p>
+                <ListGroup>
+                    {this.state.friendlist.map((friend, index) => {
+                        return (
+                            <ListGroupItem active={this.state.activeItemId === friend.id_user} color={this.state.activeItemId === friend.id_user ? 'light' : null}
+                                key={friend.id_user} tag={Link} to={`/messages/${friend.id_user}`}
+                                action onClick={() => this.handleClick(friend.id_user)}
+                            >
+                                {friend.user_name}
+                            </ListGroupItem >
+                        )
+                    })
+                    }
+                </ListGroup>
+            </div>
+        )
+    }
 }
 function mapStateToProps(state) {
     return { auth: state.auth }
--- CMS_Cross_Music/ClientApp/src/components/MessageList.js
+++ CMS_Cross_Music/ClientApp/src/components/MessageList.js
@@ -1,57 +1,106 @@
-import React from 'react';
-import {Alert} from 'reactstrap';
+import React, { Component } from 'react';
+import { Alert } from 'reactstrap';
+import { connect } from 'react-redux';
 
 const DUMMY_DATA = [
-  {
-      autorId: 2,
-      UserName:'Pier',
-      text: 'Hey, how is it going?',
-      date:'2019-10-08',
-      time:'19:23'
-  },
-  {
-      autorId: 1,
-      UserName:'Admin',
-      text: 'Great! How about you?',
-      date:'2019-10-08',
-      time:'19:46'
-  },
-  {
-      autorId: 2,
-      UserName:'Pier',
-      text: 'Good to hear! I am great as well',
-      date:'2019-10-09',
-      time:'07:15'
-  }
+    {
+        autorId: 2,
+        UserName: 'Pier',
+        text: 'Hey, how is it going?',
+        date: '2019-10-08',
+        time: '19:23'
+    },
+    {
+        autorId: 1,
+        UserName: 'Admin',
+        text: 'Great! How about you?',
+        date: '2019-10-08',
+        time: '19:46'
+    },
+    {
+        autorId: 2,
+        UserName: 'Pier',
+        text: 'Good to hear! I am great as well',
+        date: '2019-10-09',
+        time: '07:15'
+    }
 ]
 
-class MessageList extends React.Component {
+class MessageList extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loged_user_id: '',
+            target_user_id: '',
+            msg: [],
+        };
+    }
+
+    async componentDidMount() {
+        if (this.props.auth.isAuthenticated) {
+            let user_id = this.props.auth.user.idUser;
+            let list = [];
+            let friend_id = this.props.match.params.id;
+            await fetch('api/Msgs?$expand=userIdAuthorNavigation,userIdTargerNavigation&$filter=(userIdAuthor eq ' + user_id + ' and userIdTarger eq ' + friend_id +
+                ' )  or (userIdAuthor eq ' + friend_id + ' and userIdTarger eq ' + user_id + ' )')
+                .then(response => response.json())
+                .then(data => {
+                    list = data;
+                });
+
+
+            var msg_list = [];
+            console.log(list);
+            for (var i = 0; i < list.length; i++) {
+                let uf = list[i];
+                let u = {
+                    autorId: uf.UserIdAuthor,
+                    UserName: uf.UserIdAuthorNavigation.UserName,
+                    //UserName of target is UserIdTargerNavigation.UserName
+                    text: uf.Text,
+                    date: uf.MsgDate
+                }
+                msg_list.push(u);
+            }
+            console.log(msg_list);
+            this.setState({ msg: msg_list });
+
+        }
+    }
+
+
     render() {
-      let previousDate = '1900-01-01';
+        let previousDate = '1900-01-01';
         return (
-          <div className='border rounded'>
-            <p>MessageList</p>
-            {DUMMY_DATA.map((message, index) => {
-                return (
-                  <div ><p className='d-flex justify-content-center text-dark'> {previousDate!= message.date? previousDate = message.date : ''}</p> 
-                  
-                    <div className={`d-flex p-1 bd-highlight justify-content${message.autorId === 1? '-end':'-start'}`}>
-                      <div className='d-flex flex-column bd-highlight'>
-                        <div className='d-flex bd-highlight'>
-                          <div className = 'mr-auto  bd-highlight text-dark' >{message.UserName}</div>
-                          <div className = 'bd-highlight text-dark' >{message.time}</div>
+            <div className='border rounded'>
+                <p>MessageList</p>
+                {DUMMY_DATA.map((message, index) => {
+                    return (
+                        <div ><p className='d-flex justify-content-center text-dark'> {previousDate != message.date ? previousDate = message.date : ''}</p>
+
+                            <div className={`d-flex p-1 bd-highlight justify-content${message.autorId === 1 ? '-end' : '-start'}`}>
+                                <div className='d-flex flex-column bd-highlight'>
+                                    <div className='d-flex bd-highlight'>
+                                        <div className='mr-auto  bd-highlight text-dark' >{message.UserName}</div>
+                                        <div className='bd-highlight text-dark' >{message.time}</div>
+                                    </div>
+                                    <Alert style={{ display: 'inline-block' }}
+                                        color={message.autorId === 1 ? 'info' : 'light'}>{message.text}
+                                    </Alert>
+                                </div>
+                            </div>
                         </div>
-                        <Alert style={{display : 'inline-block'}}
-                          color = {message.autorId === 1? 'info':'light'}>{message.text} 
-                        </Alert>
-                      </div>
-                    </div>
-                  </div>
-                )               
-            })}
-          </div>
+                    )
+                })}
+            </div>
         )
     }
 }
 
-export default MessageList
+function mapStateToProps(state) {
+    return { auth: state.auth }
+}
+
+export default connect(mapStateToProps)(MessageList);
