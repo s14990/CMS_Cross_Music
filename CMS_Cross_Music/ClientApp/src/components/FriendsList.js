48a0b5f6d2684052d8a8436a04e02228e88934f7
s14990 <s14990@pjwtk.edu.pl>
2019-11-18 20:59:24 +0100

    added show_post

--- CMS_Cross_Music/ClientApp/src/components/FriendsList.js
+++ CMS_Cross_Music/ClientApp/src/components/FriendsList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 const DUMMY_DATA = [
   {
@@ -22,9 +23,33 @@ class FriendsList extends Component {
     this.handleClick = this.handleClick.bind(this);
 
     this.state = {
-      activeItemId: -1
+        activeItemId: -1,
+        ufl: [],
+        friendlist: []
     };
+    }
+
+
+    componentDidMount() {
+        fetch('friendlists?$expand=ufl($expand=userIdUserNavigation)&$select=Ufl&$filter=UserIdUser eq 1' + this.props.auth.idUser)
+          .then(response => response.json())
+            .then(data => {
+              this.setState({
+                  ufl: data[0]
+              });
+
+          });
+        var user_list = [];
+        this.sate.ufl.map(uf => {
+            let u;
+            u.id_user = uf.UserUserIdUserNavigation.idUser;
+            u.user_name = uf.UserUserIdUserNavigation.UserName;
+            user_list.push(u);
+        });
+        this.setState({ friendlist: user_list });
   }
+
+
   handleClick(id) {
      //   console.log('handleClick:', {idTopic, headLine});
         this.setState({ 
@@ -37,9 +62,9 @@ class FriendsList extends Component {
       return (
         <div className='border rounded'>
           <p className='text-dark'>FriendsList</p>
-          <ListGroup>
-          {DUMMY_DATA.map((friend, index) => {
-            return (
+              <ListGroup>
+                  {this.state.friendlist.map((friend, index) => {
+                return (
               <ListGroupItem  active= {this.state.activeItemId === friend.id_user} color={this.state.activeItemId === friend.id_user?'light':null}
                 key={friend.id_user} tag={Link} to={`/messages/${friend.id_user}` }
                 action onClick={()=> this.handleClick(friend.id_user)}
@@ -54,5 +79,8 @@ class FriendsList extends Component {
       )
   }
 }
+function mapStateToProps(state) {
+    return { auth: state.auth }
+}
 
-export default FriendsList
+export default connect(mapStateToProps)(FriendsList);
--- CMS_Cross_Music/ClientApp/src/components/Show_Post.js
+++ CMS_Cross_Music/ClientApp/src/components/Show_Post.js
@@ -1,5 +1,6 @@
 ﻿import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import SanitizedHTML from 'react-sanitized-html';
 
 
 class Show_Post extends Component {
@@ -7,15 +8,61 @@ class Show_Post extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { id: '' }
+        this.state = {
+            IdPost: '',
+            comments: [],
+            PostHtml: '',
+            PostDate: '',
+            mediafile: '',
+        }
+        this.getShortDate = this.getShortDate.bind(this);
+    }
+
+    componentDidMount() {
+        let post_id = this.props.match.params.id;
+        fetch('/api/Mediaposts?$expand=comment,userIdUserNavigation,mediaFileIdFileNavigation&$filter=IdPost eq ' + post_id)
+            .then(response => response.json())
+            .then(data => data[0])
+            .then(data => {
+                console.log(data);
+                this.setState({
+                    IdPost: data.IdPost,
+                    PostHtml: data.PostHtml,
+                    PostDate: new Date(data.PostDate),
+                    comments: data.comment,
+                    user: data.UserIdUserNavigation,
+                    mediafile: data.MediaFileIdFileNavigation
+                });
+            });
+        /*
+        this.state.comments: [
+                {
+                "IdComment": 1,
+                "CommentHtml": "AAA",
+                "CommentDate": "2019-11-18T12:12:09.77"
+                }
+                ...
+        */
+
+    }
+
+
+    getShortDate(json_date) {
+        let full_date = new Date(json_date);
+        return full_date.toLocaleDateString();
     }
 
 
     render() {
         return (
             <div>
-                <h1>Post_Id: {this.props.post.idPost}</h1> 
-                <div dangerouslySetInnerHTML={{ __html: this.props.post.postHtml }}></div>
+                <h1>Post_Id: {this.state.IdPost}</h1>
+                <p> {this.getShortDate(this.state.PostDate)}{this.getShortDate(this.state.PostDate)}</p>
+                <SanitizedHTML
+                    allowedAttributes={{ 'a': ['href'] }}
+                    allowedTags={['a', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'span','ul','li']}
+                    html={this.state.PostHtml}
+                />
             </div>
         );
     }
