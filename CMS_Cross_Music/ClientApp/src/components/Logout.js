4be75e110573b3820a9c1e7772e013513bab5a71
s14990 <s14990@pjwtk.edu.pl>
2019-11-18 09:25:58 +0100

    Finished Logout

---
+++ CMS_Cross_Music/ClientApp/src/components/Logout.js
@@ -0,0 +1,45 @@
+﻿import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import validator from 'validator';
+import { actionCreators } from '../store/user_Auth';
+import { Container, Form, Input, Label, FormGroup, Row, Button } from 'reactstrap';
+
+class Logout extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        var end_date = new Date('October 15, 1996 05:35:32');
+        fetch("api/Sesns/" + this.props.sesn.idSesn, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                idSesn: this.props.sesn.idSesn,
+                startDate: this.props.sesn.startDate,
+                endDate: end_date.toJSON(), 
+                userIdUser: this.props.sesn.userIdUser
+            })
+        });
+        this.props.logoutUser();
+    }
+
+    render() {
+        return (
+            <div>
+                <Redirect to='/' />
+            </div>
+        );
+    }
+
+}
+
+export default connect(
+    state => state.auth,
+    dispatch => bindActionCreators(actionCreators, dispatch)
+)(Logout);
