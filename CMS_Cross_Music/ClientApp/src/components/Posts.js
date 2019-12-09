8d7472bb94867853ba75b9b2ed7f77ef71756d95
GeorgyBelski <45123704+GeorgyBelski@users.noreply.github.com>
2019-12-09 14:48:15 +0100

    Add Search by Title

--- CMS_Cross_Music/ClientApp/src/components/Posts.js
+++ CMS_Cross_Music/ClientApp/src/components/Posts.js
@@ -10,7 +10,12 @@ class Posts extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { posts: [] }
+        this.state = { 
+            query: '',
+            results: [],
+            posts: [] 
+        }
+
         fetch('api/Mediaposts?$expand=userIdUserNavigation,mediaFileIdFileNavigation')
             .then(response => response.json())
             .then(data => {
@@ -21,12 +26,43 @@ class Posts extends Component {
             });
     }
 
+    getInfo = () => {
+        fetch(`api/Mediaposts?$expand=userIdUserNavigation,mediaFileIdFileNavigation&$filter=contains(PostTitle, '${this.state.query}' )`)
+        .then(response => response.json())
+        .then(data => {
+            this.setState({
+                posts: data
+
+            });
+        });
+    }
+    
+    handleInputChange = () => {
+        this.setState({
+          query: this.search.value
+        }, () => {
+          if (this.state.query && this.state.query.length > 1) {
+            if (this.state.query.length % 2 === 0) {
+              this.getInfo()
+            }
+          } else if (!this.state.query) {
+          }
+        })
+      }
 
     render() {
         console.log( this.state.posts)
         return (
             <div>
                 <h2>All Posts</h2>
+                <form>
+                    <input
+                        placeholder="Search for..."
+                        ref={input => this.search = input}
+                        onChange={this.handleInputChange}
+                    />
+                    <p>{this.state.query}</p>
+                </form>
                 <div className='d-flex flex-wrap'>
                     {this.state.posts.map(post =>
                     <div key = {post.IdPost}>
