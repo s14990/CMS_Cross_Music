c7039d7d4a4b8b75a7791086c5fa5195d13ba37a
s14990 <s14990@pjwstk.edu.pl>
2020-01-12 20:54:38 +0100

    Added option to add friend

--- CMS_Cross_Music/ClientApp/src/components/FriendsList.js
+++ CMS_Cross_Music/ClientApp/src/components/FriendsList.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import { ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actionCreators } from '../store/selected_user';
+import Select from 'react-select';
 
 class FriendsList extends Component {
 
@@ -11,31 +12,62 @@ class FriendsList extends Component {
         super(props);
 
         this.handleClick = this.handleClick.bind(this);
-
+        this.handleChange = this.handleChange.bind(this);
+        this.handleAdd = this.handleAdd.bind(this);
+        this.prep = this.prep.bind(this);
         this.state = {
             activeItemId: -1,
             ufl: [],
-            friendlist: []
+            friendlist: [],
+            possible_friends: [],
+            not_friend_ids: [],
+            current_user: 0,
+            selectedOption: ''
         };
+
     }
 
 
     async componentDidMount() {
+       await this.prep();
+    }
+
+
+    async prep() {
         let user_id = this.props.auth.user.idUser;
-        await fetch('api/friendlists?$expand=ufl($expand=userIdUserNavigation)&$select=Ufl&$filter=UserIdUser eq ' + user_id)
+        await fetch('api/friendlists?$expand=ufl($expand=userIdUserNavigation)&$filter=UserIdUser eq ' + user_id)
             .then(response => response.json())
             .then(data => {
+                let result = data[0].Ufl.map(a => a.UserIdUser);
                 this.setState({
-                    ufl: data[0].Ufl
+                    ufl: data[0].Ufl, not_friend_ids: result, current_user: user_id, current_user_fl: data[0].IdFl
                 });
 
             });
 
 
+        await fetch('api/Usrs?$expand=friendlist&$filter=idUser in (' + this.state.not_friend_ids.join(',') + ') eq false and idUser ne ' + user_id)
+            .then(response => response.json())
+            .then(data => {
+                var mapa = data;
+                console.log(mapa);
+                var searchList = mapa.map(
+                    usr => {
+                        return {
+                            value: usr,
+                            label: usr.UserName,
+                        }
+                    }
+                );
+                this.setState({
+                    searchList
+                });
+
+            });
+
         var user_list = [];
         var list = this.state.ufl;
-        
-        console.log(this.state.ufl);
+
         for (var i = 0; i < list.length; i++) {
             let uf = list[i];
             let u = {
@@ -45,12 +77,53 @@ class FriendsList extends Component {
             user_list.push(u);
         }
         this.setState({ friendlist: user_list });
-
+        this.setState({ selectedOption: '' });
     }
-
 
     handleClick(id) {
         this.props.selectUser(id);
+    }
+
+    handleChange = selectedOption => {
+        this.setState({ selectedOption })
+    }
+
+    handleAdd() {
+        var req1 = JSON.stringify({
+            active: true,
+            friendlistIdFl: this.state.current_user_fl,
+            userIdUser: this.state.selectedOption.value.IdUser
+        });
+        console.log(req1);
+        var req2 = JSON.stringify({
+            active: true,
+            friendlistIdFl: this.state.selectedOption.value.Friendlist.IdFl,
+            userIdUser: this.state.current_user
+        });
+        fetch("/api/ufls", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: req1
+        });
+        fetch("/api/ufls", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: req2
+        });
+        console.log(req2);
+        this.prep();
+        /*
+        fetch("/api/Zamowienies", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: req
+            */
     }
 
     render() {
@@ -70,6 +143,14 @@ class FriendsList extends Component {
                     })
                     }
                 </ListGroup>
+                <Select
+                    value={this.state.selectedOption}
+                    options={this.state.searchList}
+                    onChange={this.handleChange}
+                />
+                {this.state.selectedOption &&
+                    <Button color="warning" onClick={this.handleAdd}>Dodaj</Button>
+                }
             </div>
         )
     }
