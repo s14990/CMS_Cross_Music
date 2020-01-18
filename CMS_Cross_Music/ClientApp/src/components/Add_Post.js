2a9a9dd953c768ba01062abfa88e5cc31c0b21c2
s14990 <s14990@pjwstk.edu.pl>
2020-01-18 14:27:58 +0100

    Created File chooser

--- CMS_Cross_Music/ClientApp/src/components/Add_Post.js
+++ CMS_Cross_Music/ClientApp/src/components/Add_Post.js
@@ -2,14 +2,17 @@
 import { connect } from 'react-redux';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { Button } from 'reactstrap';
+import FCPopUp from './FCPopUp';
 
 class Add_Post extends Component {
 
 
     constructor(props) {
         super(props);
-        this.state = { text: '' };
+        this.state = { text: '', open: false,file: '' };
         this.handleChange = this.handleChange.bind(this);
+        this.choose_file = this.choose_file.bind(this);
         this.refresh = this.refresh.bind(this);
     }
 
@@ -24,10 +27,11 @@ class Add_Post extends Component {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                postHtml: this.state.text,
                 postDate: new Date().toJSON(),
-                mediaFileIdFile: 1,
-                userIdUser: 1,
+                mediaFileIdFile: this.state.file.idFile,  
+                userIdUser: this.props.auth.user.idUser,
+                postDescription: "TestTest",
+                postTitle: "Title",
             }) 
         }).then(setTimeout(this.refresh, 300));
 
@@ -37,18 +41,33 @@ class Add_Post extends Component {
         this.props.history.push("/");
     }
 
+    showModal() {
+        this.setState({ open: true });
+    }
 
+    closeModal() {
+        this.setState({ open: false });
+    }
+
+    choose_file(file) {
+        this.setState({file: file, open: false})
+    }
 
     render() {
         return (
             <div>
-            <h1>Add Post</h1>
+                <h1>Add Post</h1>
+                {this.state.file && 
+                    <p>Chosen file: {this.state.file.flName}</p>
+                }
             <ReactQuill value={this.state.text}
                     onChange={this.handleChange} />
-            <button onClick={this.uploadHandler}>Upload</button>
+                <Button onClick={this.uploadHandler}>Upload</Button>
+                <Button onClick={this.showModal.bind(this)} > Choose Video</Button>
+                <FCPopUp isopen={this.state.open} hide={this.closeModal.bind(this)} accept={this.choose_file} />
             </div>
         );
     }
 }
 
-export default connect()(Add_Post);
+export default connect(state => state)(Add_Post);
