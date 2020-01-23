b93f0f7da5f1dab3ff62320846be776f9431f88d
s14990 <s14990@pjwstk.edu.pl>
2020-01-23 23:01:24 +0100

    controllers for tags

---
+++ CMS_Cross_Music/Controllers/PtsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CMS_Cross_Music.Models;
+using Microsoft.AspNet.OData;
+
+namespace CMS_Cross_Music.Controllers
+{
+    [EnableQuery]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PtsController : ControllerBase
+    {
+        private readonly CrossMusicContext _context;
+
+        public PtsController(CrossMusicContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Pts
+        [HttpGet]
+        public IEnumerable<Pt> GetPt()
+        {
+            return _context.Pt;
+        }
+
+        // GET: api/Pts/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPt([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var pt = await _context.Pt.FindAsync(id);
+
+            if (pt == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pt);
+        }
+
+        // PUT: api/Pts/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPt([FromRoute] int id, [FromBody] Pt pt)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != pt.IdPt)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pt).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PtExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pts
+        [HttpPost]
+        public async Task<IActionResult> PostPt([FromBody] Pt pt)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Pt.Add(pt);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPt", new { id = pt.IdPt }, pt);
+        }
+
+        // DELETE: api/Pts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePt([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var pt = await _context.Pt.FindAsync(id);
+            if (pt == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pt.Remove(pt);
+            await _context.SaveChangesAsync();
+
+            return Ok(pt);
+        }
+
+        private bool PtExists(int id)
+        {
+            return _context.Pt.Any(e => e.IdPt == id);
+        }
+    }
+}
---
+++ CMS_Cross_Music/Controllers/TagsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CMS_Cross_Music.Models;
+using Microsoft.AspNet.OData;
+
+namespace CMS_Cross_Music.Controllers
+{
+    [EnableQuery]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagsController : ControllerBase
+    {
+        private readonly CrossMusicContext _context;
+
+        public TagsController(CrossMusicContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tags
+        [HttpGet]
+        public IEnumerable<Tag> GetTag()
+        {
+            return _context.Tag;
+        }
+
+        // GET: api/Tags/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTag([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var tag = await _context.Tag.FindAsync(id);
+
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tag);
+        }
+
+        // PUT: api/Tags/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTag([FromRoute] int id, [FromBody] Tag tag)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != tag.IdTag)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tag).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TagExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tags
+        [HttpPost]
+        public async Task<IActionResult> PostTag([FromBody] Tag tag)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Tag.Add(tag);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTag", new { id = tag.IdTag }, tag);
+        }
+
+        // DELETE: api/Tags/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTag([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var tag = await _context.Tag.FindAsync(id);
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tag.Remove(tag);
+            await _context.SaveChangesAsync();
+
+            return Ok(tag);
+        }
+
+        private bool TagExists(int id)
+        {
+            return _context.Tag.Any(e => e.IdTag == id);
+        }
+    }
+}
